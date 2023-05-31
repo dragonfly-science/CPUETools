1be99735847b28a780ca243e93abb0d7dd07cc14
Kyuhan Kim <kh2064@gmail.com>
2023-06-01 10:29:33 +1200

    include any(.)

--- R/WCPFC_map.R
+++ R/WCPFC_map.R
@@ -51,8 +51,8 @@ WCPFC_map_quant <- function(data,
   } else {
     #browser()
     g1 <- ggplot(pdat)  +
-      {if (class(pdat)!='sf') geom_tile(aes(x=!!lon,y=!!lat,fill=!!labq))} +
-      {if (class(pdat)=='sf') geom_sf(aes(fill=!!labq))}+
+      {if (any(class(pdat)!='sf')) geom_tile(aes(x=!!lon,y=!!lat,fill=!!labq))} +
+      {if (any(class(pdat)=='sf')) geom_sf(aes(fill=!!labq))}+
       scale_fill_viridis_c(option='E', trans = trans, limits = limits) +
       scale_colour_viridis_c(option='E', trans = trans, limits = limits)
   }
